3bfb690f0b533a06ece11d4c0dfc46629bc52ee8
gao01java <60835579+gao01java@users.noreply.github.com>
2021-06-09 15:36:06 +0900

    ㄷㄷ

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton menuB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        menuB = (ImageButton) findViewById(R.id.menuButton);
+        menuB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
